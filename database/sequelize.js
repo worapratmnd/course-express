910e880cb609ccdd92e3dc23c58625daecbbb15a
worapratmnd <woraprat_2010@hotmail.com>
2021-04-22 21:54:41 +0700

    update env

--- database/sequelize.js
+++ database/sequelize.js
@@ -1,9 +1,16 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
-module.exports = new Sequelize('postgres', 'postgres', 'password', {
-  host: 'localhost',
-  dialect: 'postgres',
-  define: {
-    timestamps: false
-  }
-});
+console.log("process.env.POSTGRES_HOST: ", process.env.POSTGRES_HOST);
+
+module.exports = new Sequelize(
+  process.env.DATABASE || 'postgres',
+  process.env.POSTGRES_USERNAME || 'postgres',
+  process.env.POSTGRES_PASSWORD || 'password',
+  {
+    host: process.env.POSTGRES_HOST || 'localhost',
+    port: process.env.POSTGRES_PORT || '5432',
+    dialect: 'postgres',
+    define: {
+      timestamps: false
+    }
+  });
